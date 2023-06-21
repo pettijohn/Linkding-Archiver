f4d4095621efa719bd7f619445eac88e36812d26
pettijohn <632844+pettijohn@users.noreply.github.com>
2023-06-21 13:52:21 -0700

    Small improvements

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,64 @@
 import puppeteer from 'puppeteer-core';
-import { KnownDevices, ScreenshotClip } from 'puppeteer-core';
-import * as fs from 'fs';
+import { KnownDevices, Page, Browser, CDPSession } from 'puppeteer-core';
+import * as fs from 'node:fs';
+import * as url from 'node:url';
 
+/** 
+ * Main - Spin up Chromium and attach a debug session 
+ */
 (async () => {
+  const browser = await puppeteer.launch({ headless: "new", executablePath: process.env.PUPPETEER_EXECUTABLE_PATH });
   // https://stackoverflow.com/questions/50884499/how-can-i-include-mobile-device-details-in-headers-when-making-a-request
   // https://github.com/puppeteer/puppeteer/blob/main/docs/api/puppeteer.knowndevices.md
   const device = KnownDevices['iPhone 11'];
   
-  const browser = await puppeteer.launch({ headless: "new", executablePath: process.env.PUPPETEER_EXECUTABLE_PATH });
   const page = await browser.newPage();
   await page.emulate(device);
 
-  await page.goto('https://theverge.com');
   const session = await page.target().createCDPSession();
-  await session.send('Page.enable');
+  
+  await captureArchive(page, session, 'https://www.theverge.com');
+  await page.close();  
+  await browser.close();
+})();
 
+/**
+ * Capture the page as MHTML and above-the-fold thumbnail 
+ */
+async function captureArchive(page: Page, session: CDPSession, url: string) {
+  //await session.send('Page.enable');
+  await page.goto(url);
+  const parsedUrl = new URL(url);
+  
   //Capture a square thumbnail screenshot
-  await page.screenshot({ path: "thumbnail.png", clip: { height: device.viewport.width, width: device.viewport.width, x: 0, y: 0 } });
+  await page.screenshot({ path: `${parsedUrl.hostname}.png` }); //, clip: { height: device.viewport.width, width: device.viewport.width, x: 0, y: 0 } 
+  // await page.screenshot({ path: `${normalizeUrlToFilename(url)}.png` }); //, clip: { height: device.viewport.width, width: device.viewport.width, x: 0, y: 0 } 
 
   // Scroll to end to trigger lazy-load images
   await autoScroll(page);
 
+  // https://github.com/puppeteer/puppeteer/issues/3575#issuecomment-447258318
+  // https://vanilla.aslushnikov.com/?Page.captureSnapshot
   const { data } = await session.send('Page.captureSnapshot');  
-  fs.writeFileSync('capture.mht', data);
+  fs.writeFileSync(`${normalizeUrlToFilename(url)}.mht`, data);
+}
 
-  await browser.close();
-})();
+/**
+ * TODO - design filesystem layout 
+ */
+function normalizeUrlToFilename(url: string): string {
+// Replace all characters that are not a-z nor 0-9 with _
+const parsedUrl = new URL(url);
+  const disallowed = /[^a-z0-9]/ig;
+  return url.replaceAll(disallowed, '_');
+}
 
-
-
-async function autoScroll(page) {
-  await page.evaluate(async () => {
+/**
+ * Scroll the entire page to trigger lazy-load images 
+ */
+async function autoScroll(page: Page) {
+// https://stackoverflow.com/questions/51529332/puppeteer-scroll-down-until-you-cant-anymore
+await page.evaluate(async () => {
     await new Promise<void>((resolve) => {
       var totalHeight = 0;
       var distance = 100;
